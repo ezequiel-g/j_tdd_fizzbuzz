4750cf3a69df0361f474b21b64f379b793ddd5a7
ezequiel-g <ezequielghione@gmail.com>
2023-05-20 00:08:49 -0300

    add FizzBuzzTest

---
+++ src/test/java/poc/tdd/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package poc.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    @Test
+    void test1() {
+        assertEquals(1, 1);
+    }
+
+}
